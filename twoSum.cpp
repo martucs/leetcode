8d4fde9a94f4d6421fcb137a87e40e0a229c3b3b
Marta <martalopezcab@gmail.com>
2026-02-11 10:57:21 +0100

    twoSum with unit tests and cleaner

--- twoSum.cpp
+++ twoSum.cpp
@@ -1,26 +1,23 @@
 #include <iostream>
 #include <vector>
 #include <strings.h>
+#include <cassert>
 
 std::vector<int> twoSum(std::vector<int> vec, int target)
 {
     std::vector<int> sol;
-    
-    std::cout << "target = " << target << std::endl;
 
     size_t i = 0;
-    int base = 0;
-    while ((size_t)base < vec.size() - 1)
+    size_t base = 0;
+    while (base < vec.size() - 1)
     {
         i = 0;
-        std::cout << "base = " << base << std::endl;
+        // std::cout << "base = " << base << std::endl;
         while (base + i + 1 != vec.size())
         {
-            std::cout << "i = " << i << std::endl;
+            // std::cout << "i = " << i << std::endl;
             if (vec[base] + vec[base + i + 1] == target)
             {
-                // std::cout << "found solution in i = " << i << std::endl;
-                // std::cout << vec[i] << " + " << vec[i + 1] << " = " << vec[i] + vec[i + 1] << std::endl;
                 sol.push_back(base);
                 sol.push_back(base + i + 1);
                 return (sol);
@@ -29,24 +26,38 @@ std::vector<int> twoSum(std::vector<int> vec, int target)
         }
         base++;
     }
-    std::cout << "did NOT find solution" << std::endl;
     return (sol);
 }
 
 void printVector(std::vector<int> vec)
 {
-    std::vector<int>::iterator it = vec.begin();
     if (vec.empty())
-        std::cout << "vector is empty\n";
-    while (!vec.empty() && it < vec.end())
     {
-        std::cout << *it << " ";
-        it++;
+        std::cout << "error: vector is empty\n";
+        return ;
     }
+    std::cout << "[" ;
+    for (size_t i = 0; i < vec.size(); ++i)
+    {
+        std::cout << vec[i];
+        if (i != vec.size() - 1)
+            std::cout << ",";
+    }
+    std::cout << "]" << std::endl;
+}
+
+void    runDebugTest(std::vector<int> nums, int target,  std::vector<int> sol)
+{
+    std::cout << "target = " << target << std::endl;
+    sol = twoSum(nums, target);
+    if (!sol.empty())
+        printVector(sol);
+    else
+        std::cout << "error: did NOT find solution" << std::endl;
     std::cout << std::endl;
 }
 
-int main(void)
+void    debugTests(void)
 {
     std::vector<int> nums1 = {2, 7, 11, 15};
     int target1 = 9;
@@ -60,29 +71,51 @@ int main(void)
     std::vector<int> nums4 {1,3,3,4};
     int target4 = 7;
 
-    std::vector<int> sol;
+    std::vector<int> sol = {0,1};
+    runDebugTest(nums1, target1, sol);
 
-    sol = twoSum(nums1, target1);
-    if (!sol.empty())
-        printVector(sol);
+    sol = {1,2};
+    runDebugTest(nums2, target2, sol);
     
-    std::cout << std::endl;
+    sol = {0,1};
+    runDebugTest(nums3, target3, sol);
+    
+    sol = {0,3};
+    runDebugTest(nums4, target4, sol);
+}
 
-    sol = twoSum(nums2, target2);
-    if (!sol.empty())
-        printVector(sol);
-   
-    std::cout << std::endl;
+void    tests()
+{
+    std::vector<int> nums1 = {2, 7, 11, 15};
+    int target1 = 9;
 
-    sol = twoSum(nums3 ,target3);
-    if (!sol.empty())
-        printVector(sol);
+    std::vector<int> nums2 = {3, 2, 4};
+    int target2 = 6;
+    
+    std::vector<int> nums3= {3,3};
+    int target3= 6;
 
-    std::cout << std::endl;
+    std::vector<int> nums4 {1,3,3,4};
+    int target4 = 5;
 
-    sol = twoSum(nums4,target4);
-    if (!sol.empty())
-        printVector(sol);
+    std::vector<int> sol {0,1};
+    assert(twoSum(nums1, target1) == sol);
+    
+    sol = {1,2};
+    assert(twoSum(nums2, target2) == sol);
+
+    sol = {0,1};
+    assert(twoSum(nums3, target3) == sol);
+
+    sol = {0,3};
+    assert(twoSum(nums4, target4) == sol);
+}
+
+int main(void)
+{
+    // debugTests();
+    tests();
+    std::cout << "twoSum() passed all tests successfully!\n";
 
     return (0);
 }
